6bcd6c28f0547c89239ff0870b5898de7e821e75
PJ Frias <peejfrias@gmail.com>
2017-11-08 11:13:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ function logDriversByHometown(drivers,hometown){
 }
 
 function driversByRevenue(drivers) {
+  drivers.sort(function(a,b) {
+    return parseFloat(a.revenue) - parseFloat(b.revenue);
+  })
+}
+
+/*
   drivers.sort(function (driver1, driver2){
     const rev1 = driver1.revenue;
     const rev2 = driver2.revenue;
@@ -23,8 +29,9 @@ function driversByRevenue(drivers) {
     console.log(rev2)
     debugger;
     //return driver1.revenue - driver2.revenue;
-    return driver1.localeCompare(driver2);
+    return ${rev1}.localeCompare(driver2);
   })
   console.log(drivers)
   debugger;
-}
+  */
+
