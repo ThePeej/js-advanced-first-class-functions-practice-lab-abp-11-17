8d3845a7849283c60ad8bd2b6bcbff5da8189051
PJ Frias <peejfrias@gmail.com>
2017-11-08 11:05:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,13 @@ function logDriversByHometown(drivers,hometown){
 
 function driversByRevenue(drivers) {
   drivers.sort(function (driver1, driver2){
-    console.log(driver1.revenue);
-    console.log(driver2.revenue);
+    const rev1 = driver1.revenue;
+    const rev2 = driver2.revenue;
+    console.log(rev1)
+    console.log(rev2)
     debugger;
     //return driver1.revenue - driver2.revenue;
-    return driver1[revenue].localeCompare(driver2[revenue]);
+    return rev1.localeCompare(rev2);
   })
   console.log(drivers)
   debugger;
